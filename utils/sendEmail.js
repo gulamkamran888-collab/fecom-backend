668383593d17184839e124de617f1c6e4a18c17e
gulamkamran888-collab <gulamkamran888@gmail.com>
2026-02-16 21:54:16 +0100

    email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,45 @@
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: `"Fcom Support" <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     html: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// export default sendEmail;
+
 import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.SMTP_LOGIN,
+      pass: process.env.SMTP_PASSWORD,
     },
   });
 
-  const mailOptions = {
-    from: `"Fcom Support" <${process.env.EMAIL_USER}>`,
+  await transporter.sendMail({
+    from: `"Fcom Support" <${process.env.SMTP_LOGIN}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
 
 export default sendEmail;
