358a8c447a8671f7015589c66fb280a9081b0e23
gulamkamran888-collab <gulamkamran888@gmail.com>
2026-02-17 23:19:29 +0100

    emil

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -21,34 +21,54 @@
 
 // export default sendEmail;
 
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
+
+// const sendEmail = async (options) => {
+//   const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "api-key": process.env.SMTP_API,
+//     },
+//     body: JSON.stringify({
+//       sender: {
+//         name: "Fcom Support",
+//         email: "fsstecom@gmail.com",
+//       },
+//       to: [
+//         {
+//           email: options.email,
+//         },
+//       ],
+//       subject: options.subject,
+//       htmlContent: options.message,
+//     }),
+//   });
+
+//   const data = await response.json();
+
+//   if (!response.ok) {
+//     throw new Error(data.message || "Email failed");
+//   }
+// };
+
+// export default sendEmail;
+
+import { Resend } from "resend";
+
+const resend = new Resend(`${process.env.RESEND_API_KEY}`);
 
 const sendEmail = async (options) => {
-  const response = await fetch("https://api.brevo.com/v3/smtp/email", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "api-key": process.env.SMTP_API,
-    },
-    body: JSON.stringify({
-      sender: {
-        name: "Fcom Support",
-        email: "fsstecom@gmail.com",
-      },
-      to: [
-        {
-          email: options.email,
-        },
-      ],
-      subject: options.subject,
-      htmlContent: options.message,
-    }),
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev", // free default sender
+    to: options.email,
+    subject: options.subject,
+    html: options.message,
   });
+  console.log("RESEND FULL RESPONSE 👉", response);
 
-  const data = await response.json();
-
-  if (!response.ok) {
-    throw new Error(data.message || "Email failed");
+  if (response.error) {
+    throw new Error(response.error.message);
   }
 };
 
